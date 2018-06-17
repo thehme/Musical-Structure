4987cda23517a5bf12c6e584b1aa8573e01c9280
thehme <bcortez23@gmail.com>
2018-06-16 23:33:57 -0400

    added navigation between activities

--- app/src/main/java/com/example/android/musicalstructure/MainActivity.java
+++ app/src/main/java/com/example/android/musicalstructure/MainActivity.java
@@ -16,13 +16,69 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // attach event listener to numbers TextView
+        // attach event listener to record ImageView
         ImageView awakenMyLoveActiviy = (ImageView) findViewById(R.id.record1);
         awakenMyLoveActiviy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent numberActivityIntent = new Intent(MainActivity.this, AwakenMyLoveActivity.class);
-                startActivity(numberActivityIntent);
+                Intent activityIntent = new Intent(MainActivity.this, AwakenMyLoveActivity.class);
+                startActivity(activityIntent);
+            }
+        });
+        ImageView becauseTheInternetActivity = (ImageView) findViewById(R.id.record2);
+        becauseTheInternetActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activityIntent = new Intent(MainActivity.this, BecauseTheInternetActivity.class);
+                startActivity(activityIntent);
+            }
+        });
+        ImageView campActivity = (ImageView) findViewById(R.id.record3);
+        campActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activityIntent = new Intent(MainActivity.this, CampActivity.class);
+                startActivity(activityIntent);
+            }
+        });
+        ImageView kauaiActivity = (ImageView) findViewById(R.id.record4);
+        kauaiActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activityIntent = new Intent(MainActivity.this, KauaiActivity.class);
+                startActivity(activityIntent);
+            }
+        });
+        ImageView mmxxlActivity = (ImageView) findViewById(R.id.record5);
+        mmxxlActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activityIntent = new Intent(MainActivity.this, MmxxlActivity.class);
+                startActivity(activityIntent);
+            }
+        });
+        ImageView americaActivity = (ImageView) findViewById(R.id.record6);
+        americaActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activityIntent = new Intent(MainActivity.this, ThisIsAmericaActivity.class);
+                startActivity(activityIntent);
+            }
+        });
+        ImageView royaltyActivity = (ImageView) findViewById(R.id.record7);
+        royaltyActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activityIntent = new Intent(MainActivity.this, RoyaltyActivity.class);
+                startActivity(activityIntent);
+            }
+        });
+        ImageView epActivity = (ImageView) findViewById(R.id.record8);
+        epActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activityIntent = new Intent(MainActivity.this, RoyaltyActivity.class);
+                startActivity(activityIntent);
             }
         });
     }
