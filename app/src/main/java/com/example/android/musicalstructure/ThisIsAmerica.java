f085150f05dc3ad88d5df0b407c12c9fe6e42f20
thehme <bcortez23@gmail.com>
2018-06-16 23:12:42 -0400

    added activity and layout files for each of the records displayed in the app

---
+++ app/src/main/java/com/example/android/musicalstructure/ThisIsAmerica.java
@@ -0,0 +1,13 @@
+package com.example.android.musicalstructure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThisIsAmerica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_this_is_america);
+    }
+}
